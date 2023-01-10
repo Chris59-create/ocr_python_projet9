8962a8cc98d5998623cbf1b59cf1d8328c85221e
Chris Create <chris59create@gmail.com>
2023-01-10 19:11:53 +0100

    Update authentication views.py

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from . import forms
 
-# Create your views here.
+
+def signup_page(request):
+
+    form = forms.SignupForm()
+
+    if request.method == "POST":
+
+        form = forms.SignupForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+            login(request, user)
+
+            return redirect(request, settings.LOGIN_REDIRECT_URL)
+
+    return render(request,
+                  "authentication/signup.html",
+                  context={"form": form}
+                  )
