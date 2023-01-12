12da9eb8a57cde92e56770d20219052ce6115d5d
Chris Create <chris59create@gmail.com>
2023-01-12 11:51:36 +0100

    Give up use of built-in django LoginView and use of authentication.views.login_page

--- litreview/authentication/views.py
+++ litreview/authentication/views.py
@@ -10,28 +10,23 @@ def login_page(request):
     message = ""
 
     if request.method == "POST":
-
         form = forms.LoginForm(request.POST)
 
         if form.is_valid():
-
             user = authenticate(
                 username=form.cleaned_data["username"],
                 password=form.cleaned_data["password"]
             )
 
             if user is not None:
-
                 login(request, user)
                 message = f'Bonjour, {user.username}! Vous êtes connecté.'
-
             else:
-
                 message = 'Identifiants invalides.'
 
-        return render(
-            request, 'authentication/login.html',
-            context={'form': form, 'message': message})
+    return render(
+        request, 'authentication/login.html',
+        context={'form': form, 'message': message})
 
 
 def signup_page(request):
@@ -39,14 +34,11 @@ def signup_page(request):
     form = forms.SignupForm()
 
     if request.method == "POST":
-
         form = forms.SignupForm(request.POST)
 
         if form.is_valid():
-
             user = form.save()
             login(request, user)
-
             return redirect(request, settings.LOGIN_REDIRECT_URL)
 
     return render(request,
--- litreview/litreview/urls.py
+++ litreview/litreview/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 
 import authentication.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", LoginView.as_view(
-        template_name='authentication/login.html',
-        redirect_authenticated_user=True),
-        name="login"
-        ),
+    path("", authentication.views.login_page, name="login"),
     path("signup/", authentication.views.signup_page, name="signup")
 ]
