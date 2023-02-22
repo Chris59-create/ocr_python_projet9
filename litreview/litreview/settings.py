700cf244a3eeaf5b3ed7d3bb2d7695eafb8673ba
Chris Create <chris59create@gmail.com>
2023-02-22 11:38:53 +0100

    Changes the LOGIN_REDIRECT_URL in settings.py to feed:flow

--- litreview/litreview/settings.py
+++ litreview/litreview/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "feed:home"
+LOGIN_REDIRECT_URL = "feed:flow"
 LOGOUT_REDIRECT_URL = LOGIN_URL
 
 MEDIA_URL = '/media/'
